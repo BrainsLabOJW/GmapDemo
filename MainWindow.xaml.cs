cede07b5948c285a5d69480af2435da51c7d57ba
BrainsLabOJW <160471417+BrainsLabOJW@users.noreply.github.com>
2024-02-22 15:30:52 +0900

    지도 타입 변경, 줌인/아웃, 마커 연결하기

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,7 +24,10 @@ namespace GmapDemo
 
         GMapMarker currentMarker;
 
-        List<GMapMarker> markers = new List<GMapMarker>();
+        List<PointLatLng> points = new List<PointLatLng>();
+
+        private bool isMousePressed = false;
+        private bool isMouseMoved = false;
 
         public MainWindow()
         {
@@ -38,11 +41,18 @@ namespace GmapDemo
             mapControl.Zoom = 15;
             mapControl.ShowCenter = false;
             mapControl.DragButton = MouseButton.Left;
+            mapControl.MouseWheelZoomEnabled = true;
 
             mapControl.MouseLeftButtonDown += new MouseButtonEventHandler(mapControl_MouseLeftButtonDown);
-            setCurrentMarker();
-        }
+            mapControl.MouseLeftButtonUp += new MouseButtonEventHandler(mapControl_MouseLeftButtonUp);
+            mapControl.MouseMove += new MouseEventHandler(mapControl_MouseMove);
+            mapControl.MouseWheel += new MouseWheelEventHandler(mapControl_MouseWheel);
+            
 
+            setCurrentMarker();
+            mapComboBoxSetting();
+
+        }
 
         void setCurrentMarker()
         {
@@ -59,29 +69,115 @@ namespace GmapDemo
             currentMarker.Offset = new Point(-15, -15);
             currentMarker.ZIndex = int.MaxValue;
             mapControl.Markers.Add(currentMarker);
+            points.Add(mapControl.Position);
         }
 
-        void mapControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void mapControl_MouseWheel(object sender, MouseWheelEventArgs e)
         {
-            Point clickPoint = e.GetPosition(mapControl);
-            PointLatLng point = mapControl.FromLocalToLatLng((int)clickPoint.X, (int)clickPoint.Y);
-            GMapMarker marker = new GMapMarker(point);
-
-            marker.Shape = new Ellipse 
-            { 
-                Stroke = Brushes.Yellow,
-                StrokeThickness = 2,
-                Fill= new SolidColorBrush(Colors.Black),
-                Width = 10,
-                Height = 10
-            };
-
-            mapControl.Markers.Add(marker);
+            if(e.Delta > 0)
+            {
+                zoomSliderBar.Value += 1;
+            }
+            else
+            {
+                zoomSliderBar.Value -= 1;
+            }
         }
 
-        private void sliderBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void mapControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
+            isMousePressed = true;
         }
+
+        private void mapControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(isMousePressed)
+            {
+                isMouseMoved = true;
+            }
+            
+        }
+
+        void mapControl_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (!isMouseMoved) 
+            {
+                Point clickPoint = e.GetPosition(mapControl);
+                PointLatLng point = mapControl.FromLocalToLatLng((int)clickPoint.X, (int)clickPoint.Y);
+                GMapMarker marker = new GMapMarker(point);
+
+                marker.Shape = new Ellipse
+                {
+                    Stroke = Brushes.Yellow,
+                    StrokeThickness = 2,
+                    Fill = new SolidColorBrush(Colors.Black),
+                    Width = 10,
+                    Height = 10
+                };
+
+                mapControl.Markers.Add(marker);
+                points.Add(point);
+
+
+                PolygonTest();
+            }
+            else 
+            {
+                isMouseMoved = false;
+            }
+            isMousePressed = false;
+        }
+
+        void mapComboBoxSetting()
+        {
+            mapComboBox.Items.Add("GoogleSatelliteMap");
+            mapComboBox.Items.Add("GoogleMap");
+            mapComboBox.Items.Add("GoogleTerrainMap");
+            mapComboBox.SelectedIndex = 0;
+        }
+
+        private void mapComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            System.Console.WriteLine(mapComboBox.SelectedItem);
+
+            if(mapComboBox.SelectedIndex == 0)
+            {
+                mapControl.MapProvider = GMapProviders.GoogleSatelliteMap;
+            }
+            else if(mapComboBox.SelectedIndex == 1)
+            {
+                mapControl.MapProvider = GMapProviders.GoogleMap;
+            }
+            else if(mapComboBox.SelectedIndex == 2)
+            {
+                mapControl.MapProvider = GMapProviders.GoogleTerrainMap;
+            }
+            else
+            {
+                mapControl.MapProvider = GMapProviders.GoogleSatelliteMap;
+            }
+            
+        }
+
+        private void zoomSliderBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            System.Console.WriteLine(e.NewValue);
+            mapControl.Zoom = e.NewValue;
+        }
+
+        void PolygonTest()
+        {
+            GMapRoute route = new GMapRoute(points);
+                       
+            Path path = new Path();
+            path.Stroke = Brushes.Teal;
+            path.StrokeThickness = 1.5;
+            path.Effect = null;
+
+            route.Shape = path;
+
+            mapControl.Markers.Add(route);
+        }
+
     }
 }
